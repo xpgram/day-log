5822439b01641329b62f4cad6a39363cf44c860f
Dei Biddy <dbiddy@dbiddy.local>
2024-04-11 21:10:03 -0700

    add missing import

--- modules/logstream/LogLine.py
+++ modules/logstream/LogLine.py
@@ -1,12 +1,13 @@
 from typing import Self
 from abc import ABC, abstractmethod
+from enums.View import View
 
 class LogLine(ABC):
     "An abstract class describing an interface for log-related objects."
 
     @property
     @abstractmethod
-    def lstype(self) -> str:
+    def logstreamType(self) -> str:
         pass
 
     @abstractmethod
@@ -19,6 +20,6 @@ class LogLine(ABC):
         pass
 
     @abstractmethod
-    def render(self, view: View) -> str:    # TODO Missing import
+    def render(self, view: View) -> str:
         "Returns a string for displaying in the terminal."
         pass
