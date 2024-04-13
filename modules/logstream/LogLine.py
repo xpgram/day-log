8defe7f249bca6f1e96d4b203c28f47d203058ce
Dei Biddy <dbiddy@dbiddy.local>
2024-04-12 22:19:26 -0700

    update LogLine to match the pattern set by LogStreamAtom

--- modules/logstream/LogLine.py
+++ modules/logstream/LogLine.py
@@ -1,6 +1,7 @@
 from typing import Self
 from abc import ABC, abstractmethod
 from enums.View import View
+from state import ViewState
 
 class LogLine(ABC):
     "An abstract class describing an interface for log-related objects."
@@ -8,24 +9,35 @@ class LogLine(ABC):
     @property
     @abstractmethod
     def logstreamType(self) -> str:
-        pass
+        "The.. class of object this is?" # TODO Answer this question.
 
     @staticmethod
     @abstractmethod
     def fromInput(data: str) -> Self:
-        pass
+        "Construct a new LogStream object from user input."
 
     @staticmethod
     @abstractmethod
     def fromSave(data: str) -> Self:
-        pass
+        "Construct a new LogStream object from save data."
 
     @abstractmethod
     def save(self) -> str:
         "Returns a comprehensive string for storing in the save file."
-        pass
 
     @abstractmethod
     def render(self, view: View) -> str:
-        "Returns a string for displaying in the terminal."
-        pass
+        "Returns a render string built according to the given View type."
+
+    _renderCache: str | None = None
+
+    def cacheRender(self, view: View) -> str:
+        "Renders and caches the display string."
+        self._renderCache = self.render(view)
+
+    def display(self) -> str:
+        "Returns a string to display in the terminal."
+        if not self._renderCache:
+            self.cacheRender(ViewState.get())
+        return self._renderCache
+        
