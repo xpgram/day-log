319799c41f203b0e84226099e21e3d78f0fc6bff
Dei Biddy <dbiddy@dbiddy.local>
2024-04-12 22:13:58 -0700

    settle the inheritance relationship between logstream atoms and atom objects

--- modules/logstream/atoms/LogStreamAtom.py
+++ modules/logstream/atoms/LogStreamAtom.py
@@ -1,6 +1,7 @@
 from typing import Self
 from abc import ABC, abstractmethod
 from enums.View import View
+from state import ViewState
 
 class LogStreamAtom(ABC):
     "An abstract class describing an interface for log-related atoms."
@@ -8,19 +9,29 @@ class LogStreamAtom(ABC):
     @property
     @abstractmethod
     def logstreamType(self) -> str:
-        pass
+        "The.. class of object this is?" # TODO Answer this question.
 
     @staticmethod
     @abstractmethod
     def create(data: str | None) -> Self:
-        pass
+        "Assembles a new object of this class."
 
     @abstractmethod
     def save(self) -> str:
         "Returns a comprehensive string snippet for storing in the save file."
-        pass
 
     @abstractmethod
     def render(self, view: View) -> str:
-        "Returns a string snippet for displaying in the terminal."
-        pass
+        "Returns a render string built according to the given View type."
+
+    _renderCache: str | None = None
+
+    def cacheRender(self, view: View):
+        "Renders and caches the display string."
+        self._renderCache = self.render(view)
+
+    def display(self) -> str:
+        "Returns a string snippet for displaying somewhere in the terminal."
+        if not self._renderCache:
+            self.cacheRender(ViewState.get())
+        return self._renderCache
--- modules/logstream/atoms/Time.py
+++ modules/logstream/atoms/Time.py
@@ -5,17 +5,24 @@ This atom is for this part.
 12:31 +2,1 cras pulvinar - Mattis nunc sed blandit libero volutpat sed cras quis
 """
 
+from datetime import datetime
 from logstream.atoms.LogStreamAtom import LogStreamAtom
 
 class Time(LogStreamAtom):
   logstreamType = 'time-atom'
 
+  date: datetime
+
   @staticmethod
   def create(data):
-    pass
+    date = datetime.now()
+    return Time(date)
 
-  def save():
-    pass
+  def __init__(self, date: datetime):
+    self.date = date
+
+  def save(self):
+    return self.date.strftime('%Y %m %d %H:%M')
 
   def render(self, view):
-    pass
+    return self.date.strftime('%H:%M')
