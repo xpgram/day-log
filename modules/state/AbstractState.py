0e330a5a17e4341849a46da8e6cda5ef207444ba
Dei Biddy <dbiddy@dbiddy.local>
2024-04-12 21:11:45 -0700

    add never-to-be-finished AbtractState class

---
+++ modules/state/AbstractState.py
@@ -0,0 +1,6 @@
+from typing import Self
+from abc import ABC, abstractmethod
+
+# TODO If I ever have more states, I suppose.
+# I really don't feel like figuring out T and get() -> T right now.
+# Or how to make a class singleton so these modules can make use of inheritance.
