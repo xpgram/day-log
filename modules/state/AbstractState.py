80f99147ae0b9eb3c36737d3ffd6d4a046c4c38b
Devin Biddy <xp.gram@gmail.com>
2024-04-13 22:02:47 -0700

    add todo

--- modules/state/AbstractState.py
+++ modules/state/AbstractState.py
@@ -4,3 +4,5 @@ from abc import ABC, abstractmethod
 # TODO If I ever have more states, I suppose.
 # I really don't feel like figuring out T and get() -> T right now.
 # Or how to make a class singleton so these modules can make use of inheritance.
+
+# TODO Wait. Or should this just be an interface?
