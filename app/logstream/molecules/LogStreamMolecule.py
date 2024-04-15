6b19f3ab0358f03230863d14876ddd1d3752d031
Devin Biddy <xp.gram@gmail.com>
2024-04-15 00:05:59 -0700

    add file for abstract Molecule class; add todos

---
+++ app/logstream/molecules/LogStreamMolecule.py
@@ -0,0 +1,8 @@
+
+"""
+# TODO 
+Built in save, render, display as from Atom.
+Built in line wrapping. Or full width stringCutoff in compact mode.
+May have many atom components. Or just one.
+Built in indenting for UI things like line numbers.
+"""
