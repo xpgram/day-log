27de3f6a1c525a14104697e9084e9260aee135a8
Devin Biddy <xp.gram@gmail.com>
2024-04-14 23:35:12 -0700

    finish implementing the Category logstream atom

--- app/logstream/atoms/Category.py
+++ app/logstream/atoms/Category.py
@@ -1,25 +1,41 @@
+import re
+from app.system import Utils
 from app.logstream.atoms.LogStreamAtom import LogStreamAtom
 from app.enums.View import View
+from app.logstream.Exceptions import LogstreamReadError
 
 class Category(LogStreamAtom):
   logstreamType = 'category-atom'
 
   title: str
-  subtitle: str # TODO `adminia - firefox tweaks - [a message about what I spent my t...]`
-                # Should I bother with this? Sometimes I do this.
+  subtitle: str
 
   @staticmethod
   def create(data):
-    return Category(data)
+    tokens = data.split(' - ')
+    if not tokens[0]:
+      raise LogstreamReadError(f'Category has no title.')
+    if len(tokens) > 2:
+      raise LogstreamReadError(f'Category has too many subheadings: "{data}"')
+    
+    title = tokens[0]
+    subtitle = Utils.get(1, tokens, '')
+    return Category(title, subtitle)
   
-  def __init__(self, title: str):
+  def __init__(self, title: str, subtitle: str):
     self.title = title
+    self.subtitle = subtitle
 
+  def _getString(self):
+    "Returns the base string for saves and renders to build from."
+    separator = ' - ' if self.subtitle else ''
+    return f'{self.title}{separator}{self.subtitle}'
+  
   def save(self):
-    return self.title
+    return self._getString()
 
   def render(self, view):
+    string = self._getString()
     if view == View.Compact:
-      return self.title[:12]  # TODO ??
-    return self.title
-
+      string = Utils.stringCutoff(string, 80)
+    return string
