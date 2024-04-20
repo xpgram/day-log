067262695f246286ec51a94ecc8424ae35abbb43
Dei Biddy <dbiddy@dbiddy.local>
2024-04-19 17:32:19 -0700

    add new error type to Exceptions.py

--- app/logstream/Exceptions.py
+++ app/logstream/Exceptions.py
@@ -2,6 +2,9 @@
 class LogstreamReadError(Exception):
     "Could not parse data from save."
 
+class LogstreamInputError(Exception):
+    "Could not parse data from input."
+
 class LogstreamSaveError(Exception):
     "Could not serialize logstream object to save data."
 
