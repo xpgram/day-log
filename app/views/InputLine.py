2b83b0c68745736c627a8f5e0fe70dbd14857763
Dei Biddy <dbiddy@dbiddy.local>
2024-04-23 09:41:54 -0700

    add input line class file to handle the user prompt

---
+++ app/views/InputLine.py
@@ -0,0 +1,15 @@
+"""
+Prints this:
+> 
+
+Maybe prints this on indented note lines?
+• >
+• • >
+
+An indicator for logs in the redo-queue.
+!>
+!• >
+Yeah, I dunno. This might have to go on the Marquee.
+
+Similar to Marquee, this updates its display settings via messages in the event system.
+"""
